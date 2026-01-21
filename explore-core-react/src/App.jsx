2e5aef988c102e6ef7c523bd11da9c1875ac905f
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-21 11:31:41 +0600

    addad three item style add in this file first app css second create a object  and third add style add to element

--- explore-core-react/src/App.jsx
+++ explore-core-react/src/App.jsx
@@ -4,12 +4,11 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>React core concept</h1>
+      <Student></Student>
       <Person></Person>
       <Dream></Dream>
-      <Sports></Sports>
-      <Person></Person>
-      <Sports></Sports>
+      <Device></Device>
     </>
   )
 }
@@ -18,7 +17,10 @@ function Person(){
   const age=20
   const name = "sohag roy"
   return(
-    <h3>I am a person {name}  {age}</h3>
+    <h3 style={{
+      color: "skyBlue",
+      fontSize: "30px"
+    }}>I am a person {name}  {age}</h3>
   )
 }
 
@@ -32,9 +34,33 @@ function Sports(){
   )
 }
 
-function Dream(){
+function Device(){
   return(
-    <p>I want to be a professional web developer </p>
+    <div>
+      <h3>device:</h3>
+      <p>price: </p>
+    </div>
+  )
+}
+
+
+function Dream(){
+  const styleDream = {
+    color: "blue",
+   textAlign: "center",
+   fontSize: "20px"
+}
+  return(
+    <p style={styleDream}>I want to be a professional web developer </p>
+  )
+}
+
+function Student(){
+  return(
+    <div className='student'>
+      <p>name:</p>
+      <p>dept:</p>
+    </div>
   )
 }
 
