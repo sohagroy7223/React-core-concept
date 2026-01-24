67d766e31aa6d87aaed93df7ced2bca71da9ea97
sohagroy7223 <freelancersohagroy@gmail.com>
2026-01-24 13:45:09 +0600

    addad array of object with style in app . css

--- explore-core-react/src/App.jsx
+++ explore-core-react/src/App.jsx
@@ -1,13 +1,18 @@
-
-import './App.css'
-import ToDo from './todo'
-import Food from './todo'
-import Friend from './friend'
-
+import "./App.css";
+import ToDo from "./todo";
+import Food from "./todo";
+import Friend from "./friend";
+import Mobile from "./mobile";
 
 function App() {
+  const brands = [
+    { id: 1, name: "Samsung S24 ultra", brand: "samsung", price: "$1000" },
+    { id: 2, name: "Redmi not 13", brand: "Xiaomi", price: "$300" },
+    { id: 3, name: "iphone 16 Pro Max", brand: "Apple", price: "$1100" },
+    { id: 1, name: "walton 15 A", brand: "Walton", price: "$500" },
+  ];
 
-  const friends = ["Akash", "Samor","Ripon","Allok"]
+  // const friends = ["Akash", "Samor","Ripon","Allok"]
 
   // const time = 50
 
@@ -15,13 +20,17 @@ function App() {
     <>
       <h1>React core concept</h1>
 
-      {
+      {brands.map((brand) => (
+        <Mobile mobile={brand}></Mobile>
+      ))}
+
+      {/* {
         friends.map(friend=><Friend name = {friend}></Friend>)
-      }
+      } */}
 
       {/* <Food task ="he is okay" energy={true} ></Food>
       <Food task ="he is hungry" energy={false}></Food> */}
-      
+
       {/* <ToDo hay = "we should visit new place" status ={true} time = {time}></ToDo>
       <ToDo hay = "you should to take rest" status ={false}></ToDo>
       <ToDo hay = "we should read book" status ={true} time =""></ToDo> */}
@@ -29,7 +38,7 @@ function App() {
       <ToDo task = ' revision js' isDone = {true}></ToDo>
       <ToDo task = ' going to home ' isDone = {false}></ToDo>
       <ToDo task = ' sleeping now' isDone = {false}></ToDo> */}
-      
+
       {/* <Student name="sohag" dep="CSE"></Student>
       <Student name="sagor" dep="Marketing"></Student>
       <Person></Person>
@@ -40,87 +49,79 @@ function App() {
       <Phone name=" Redmi" price="$150"></Phone>
       <Phone name=" Samsung" price="$1050"></Phone>
       <Phone name=" Apple" price="$1250"></Phone> */}
-      
     </>
-  )
+  );
 }
-
 
 // option 1******
 
-function Phone({name,price}){
-
-  return(
-    <div className='student'>
+function Phone({ name, price }) {
+  return (
+    <div className="student">
       <h2>Mobile brand : {name}</h2>
       <h4>brand:{name} </h4>
-      <p>price: {price} </p> 
+      <p>price: {price} </p>
     </div>
-
-  )
+  );
 }
 
-
-function Student(props){
-  return(
-    <div className='student'>
+function Student(props) {
+  return (
+    <div className="student">
       <p>name: {props.name}</p>
       <p>dept: {props.dep}</p>
     </div>
-  )
+  );
 }
 
-
-
-function Device(prop){
+function Device(prop) {
   // console.log(prop)
-  return(
-    <div style={{
-      border:"2px solid green", 
-      borderRadius: "20px",
-      margin: "10px"
-    }}>
+  return (
+    <div
+      style={{
+        border: "2px solid green",
+        borderRadius: "20px",
+        margin: "10px",
+      }}
+    >
       <h3>device: {prop.name}</h3>
       <p>price:{prop.price} </p>
     </div>
-  )
+  );
 }
 
-
-function Person(){
-  const age=20
-  const name = "sohag roy"
-  return(
-    <h3 style={{
-      color: "skyBlue",
-      fontSize: "30px"
-    }}>I am a person {name}  {age}</h3>
-  )
+function Person() {
+  const age = 20;
+  const name = "sohag roy";
+  return (
+    <h3
+      style={{
+        color: "skyBlue",
+        fontSize: "30px",
+      }}
+    >
+      I am a person {name} {age}
+    </h3>
+  );
 }
 
-function Sports(){
-  return(
+function Sports() {
+  return (
     <div>
       <h1>game</h1>
       <h2>Cricket</h2>
       <p>we love to play cricket</p>
     </div>
-  )
+  );
 }
 
-
-
-function Dream(){
+function Dream() {
   const styleDream = {
     color: "blue",
-   textAlign: "center",
-   fontSize: "20px"
-}
-  return(
-    <p style={styleDream}>I want to be a professional web developer </p>
-  )
+    textAlign: "center",
+    fontSize: "20px",
+  };
+  return <p style={styleDream}>I want to be a professional web developer </p>;
 }
 
-
-
-export default App
+export default App;
---
+++ explore-core-react/src/mobile.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+export default function Mobile({ mobile }) {
+  //   console.log(mobile);
+  return (
+    <div className="brand">
+      <h2>brand : {mobile.brand}</h2>
+      <h5>name: {mobile.name}</h5>
+      <p>price {mobile.price}</p>
+    </div>
+  );
+}
